c24e92b0a6f273a3433098c963608e1ce5b58580
Jefersonalves <ferreira.jefersonn@gmail.com>
2023-11-20 21:18:32 -0300

    Formata fala senador com PEP8

--- fala_parlamentar/fala_senador.py
+++ fala_parlamentar/fala_senador.py
@@ -1,8 +1,9 @@
 import re
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
 from xml.etree import ElementTree
+
+import requests
+from bs4 import BeautifulSoup
+
 
 def get_transcricao_discurso_senado(url):
     """
@@ -19,23 +20,24 @@ def get_transcricao_discurso_senado(url):
         Dicionário com o título e a transcrição do discurso
     """
     r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    
+    soup = BeautifulSoup(r.content, "html.parser")
+
     texto = None
     titulo = None
 
     try:
-        texto_element = soup.find(class_='texto-integral')
+        texto_element = soup.find(class_="texto-integral")
         texto = texto_element.text.strip()
-        texto = re.sub(r'\xa0\xa0\xa0\xa0', '\n', texto)
-    
-        titulo_element = soup.find(class_='titulo-pagina')
+        texto = re.sub(r"\xa0\xa0\xa0\xa0", "\n", texto)
+
+        titulo_element = soup.find(class_="titulo-pagina")
         titulo = (titulo_element.text).strip()
     except:
         pass
-    
-    discurso = {'titulo': titulo, 'texto': texto}
+
+    discurso = {"titulo": titulo, "texto": texto}
     return discurso
+
 
 def get_discursos_senador(codigo_parlamentar, data_inicio, data_fim):
     """
@@ -67,29 +69,35 @@ def get_discursos_senador(codigo_parlamentar, data_inicio, data_fim):
                     }
                 ]
     """
-    
-    data_inicio = re.sub(r'-', '', data_inicio)
-    data_fim = re.sub(r'-', '', data_fim)
-    
-    #as urls dos discursos podem ser obtidos via API
-    discursos_url = 'https://legis.senado.leg.br/dadosabertos/senador/{}/discursos?dataInicio={}&dataFim={}'.format(str(codigo_parlamentar), data_inicio, data_fim)
+
+    data_inicio = re.sub(r"-", "", data_inicio)
+    data_fim = re.sub(r"-", "", data_fim)
+
+    # as urls dos discursos podem ser obtidos via API
+    discursos_url = "https://legis.senado.leg.br/dadosabertos/senador/{}/discursos?dataInicio={}&dataFim={}".format(
+        str(codigo_parlamentar), data_inicio, data_fim
+    )
     r = requests.get(discursos_url)
     root = ElementTree.fromstring(r.content)
-    
-    #obtenção das urls dos discursos
-    links_discursos_elements = root.findall('Parlamentar/Pronunciamentos/Pronunciamento/UrlTexto')
-    links_discursos = [texto.text for texto in links_discursos_elements] #variável com as urls
-    
-    #adição dos discursos numa lista contendo os seus dados 
+
+    # obtenção das urls dos discursos
+    links_discursos_elements = root.findall(
+        "Parlamentar/Pronunciamentos/Pronunciamento/UrlTexto"
+    )
+    links_discursos = [
+        texto.text for texto in links_discursos_elements
+    ]  # variável com as urls
+
+    # adição dos discursos numa lista contendo os seus dados
     list_discursos_senador = []
     for link in links_discursos:
         discurso = get_transcricao_discurso_senado(link)
-        
+
         list_discursos_senador.append(
             {
-             'titulo': discurso['titulo'],
-             'url_texto': requests.utils.requote_uri(link),
-             'transcricao': discurso['texto']
+                "titulo": discurso["titulo"],
+                "url_texto": requests.utils.requote_uri(link),
+                "transcricao": discurso["texto"],
             }
         )
 
